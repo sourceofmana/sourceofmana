9038a01a0446fedb7df4b6bf0baaea2335208d07
Reid <reidyaro@gmail.com>
2022-01-09 06:05:33 +0100

    Add network's callback on mainworld script and securize some more the main script, still need to clean it up

--- sources/MainWorld.gd
+++ sources/MainWorld.gd
@@ -1,42 +1,51 @@
 extends Node2D
 
 onready var currentMap		= null
-onready var currentPlayer	= load( "res://scenes/Presets/PC.tscn" ).instance()
+onready var currentPlayer	= load("res://scenes/Presets/PC.tscn").instance()
 
 # Debug
-func SetDefaultPlayerPosition() :
-	var defaultMap = "res://scenes/Map/012-3-0.tscn"
-	var defaultPosition = Vector2( 60, 30 )
-
-	SetPlayerInWorld( defaultMap, defaultPosition )
+func SetDefaultPlayerPosition():
+	var defaultMap = "res://maps/phatina/002-3-0.tmx"
+	var defaultPosition = Vector2(60, 40)
+	SetPlayerInWorld(defaultMap, defaultPosition)
 
 # Utils	
-func SetCameraBoundaries() :
-	if currentMap && currentPlayer :
-		var playerCamera	= currentPlayer.get_node( "PlayerCamera" )
-		var groundLayer		= currentMap.get_node( "Ground/Ground 1" )
+func SetCameraBoundaries():
+	if currentMap && currentPlayer:
+		var playerCamera	= currentPlayer.get_node("PlayerCamera")
+		var groundLayer		= currentMap.get_node("Ground 1")
 
-		var mapLimits		= groundLayer.get_used_rect()
-		var mapCellsize		= groundLayer.cell_size
+		assert(playerCamera, "Player camera not found")
+		assert(groundLayer, "Could not find a ground layer on map: " + currentMap.get_name())
 
-		playerCamera.limit_left		= mapLimits.position.x * mapCellsize.x
-		playerCamera.limit_right	= mapLimits.end.x * mapCellsize.x
-		playerCamera.limit_top		= mapLimits.position.y * mapCellsize.y
-		playerCamera.limit_bottom	= mapLimits.end.y * mapCellsize.y
+		if groundLayer && playerCamera:
+			var mapLimits		= groundLayer.get_used_rect()
+			var mapCellsize		= groundLayer.cell_size
 
-func SetPlayerInWorld( map, pos ) :
-	currentMap = load( map ).instance()
+			playerCamera.limit_left		= mapLimits.position.x * mapCellsize.x
+			playerCamera.limit_right	= mapLimits.end.x * mapCellsize.x
+			playerCamera.limit_top		= mapLimits.position.y * mapCellsize.y
+			playerCamera.limit_bottom	= mapLimits.end.y * mapCellsize.y
 
-	if currentPlayer && currentMap :
-		currentPlayer.set_position( pos * 32 )
+func SetPlayerInWorld(map, pos):
+	currentMap = load(map).instance()
+	if currentPlayer && currentMap:
+		var fringeSort = currentMap.get_node("Fringe")
+		if fringeSort:
+			currentPlayer.set_position(pos * fringeSort.cell_size)
+			fringeSort.add_child(currentPlayer)
+			add_child(currentMap)
 
-		var fringeSort = currentMap.get_node( "Fringe" )
-		fringeSort.add_child( currentPlayer )
-		add_child( currentMap )
+# Network
+func ReturnInventoryList(s_inventoryList):
+	print(s_inventoryList)
 
-# Ready
+#
 func _ready():
-	if currentMap == null :
+	if currentMap == null:
 		SetDefaultPlayerPosition()
-
 	SetCameraBoundaries()
+
+func _process(_delta):
+	if Input.is_action_just_pressed("ui_inventory"):
+		Server.FetchInventoryList("All", get_instance_id())
