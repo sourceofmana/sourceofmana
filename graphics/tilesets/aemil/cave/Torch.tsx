6750c2a3a51f58063c125a361ebfa5703b93aae9
Reid <reidyaro@gmail.com>
2022-01-10 02:21:31 +0100

    Add a torch external tileset with animation and collision support

---
+++ graphics/tilesets/aemil/cave/Torch.tsx
@@ -0,0 +1,43 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.1" name="Torch" tilewidth="96" tileheight="64" tilecount="18" columns="6">
+ <image source="torch-x3x2.png" width="576" height="192"/>
+ <tile id="5">
+  <objectgroup draworder="index" id="2">
+   <object id="5" x="42" y="50" width="12" height="12"/>
+  </objectgroup>
+  <animation>
+   <frame tileid="0" duration="60"/>
+   <frame tileid="1" duration="60"/>
+   <frame tileid="2" duration="60"/>
+   <frame tileid="3" duration="60"/>
+   <frame tileid="4" duration="60"/>
+   <frame tileid="5" duration="60"/>
+  </animation>
+ </tile>
+ <tile id="11">
+  <objectgroup draworder="index" id="3">
+   <object id="7" x="34" y="46" width="28" height="18"/>
+  </objectgroup>
+  <animation>
+   <frame tileid="6" duration="60"/>
+   <frame tileid="7" duration="60"/>
+   <frame tileid="8" duration="60"/>
+   <frame tileid="9" duration="60"/>
+   <frame tileid="10" duration="60"/>
+   <frame tileid="11" duration="60"/>
+  </animation>
+ </tile>
+ <tile id="17">
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="34" y="46" width="28" height="18"/>
+  </objectgroup>
+  <animation>
+   <frame tileid="12" duration="60"/>
+   <frame tileid="13" duration="60"/>
+   <frame tileid="14" duration="60"/>
+   <frame tileid="15" duration="60"/>
+   <frame tileid="16" duration="60"/>
+   <frame tileid="17" duration="60"/>
+  </animation>
+ </tile>
+</tileset>
