b6c36e79318ea7c7cd31108b8649c889abaf87fd
Reid <reidyaro@gmail.com>
2022-04-26 02:34:12 +0200

    Prepare the bar prefab to be replaced with the newly generic progressbar prefab

--- sources/gui/GameGui.gd
+++ sources/gui/GameGui.gd
@@ -1,57 +1,28 @@
 extends Node
 
-onready var HPBar			= $VBoxMain/HBoxTop/StatIndicator/Bars/HP/HPBar
-onready var HPText			= $VBoxMain/HBoxTop/StatIndicator/Bars/HP/HPText
-onready var ManaBar			= $VBoxMain/HBoxTop/StatIndicator/Bars/Mana/ManaBar
-
-onready var ManaText		= $VBoxMain/HBoxTop/StatIndicator/Bars/Mana/ManaText
-onready var StaminaBar		= $VBoxMain/HBoxTop/StatIndicator/Bars/Stamina/StaminaBar
-onready var StaminaText		= $VBoxMain/HBoxTop/StatIndicator/Bars/Stamina/StaminaText
+onready var hpStat			= $VBoxMain/HBoxTop/StatIndicator/Bars/HP
+onready var manaStat		= $VBoxMain/HBoxTop/StatIndicator/Bars/Mana
+onready var staminaStat		= $VBoxMain/HBoxTop/StatIndicator/Bars/Stamina
 
 onready var LevelText		= $VBoxMain/HBoxTop/StatIndicator/LevelText
 onready var ExpText			= $VBoxMain/HBoxTop/StatIndicator/ExpText
 
-func GetRatio(currentValue : int, maxValue : int) -> float:
-	var ratio = 0.0
-	if maxValue > 0:
-		ratio = float(currentValue) / maxValue * 100
-	return ratio
-
-func GetFormatedText(value : String) -> String:
-	var i : int = value.length() - 3
-	while i > 0:
-		value = value.insert(i, ",")
-		i = i - 3
-	return value
-
-func GetBarFormat(currentValue : int, maxValue : int) -> String:
-	var currentValueText = GetFormatedText(currentValue as String)
-	var maxValueText = GetFormatedText(maxValue as String)
-	return currentValueText + " / " + maxValueText
 
 func GetPercentFormat(value : int) -> String:
 	return "%.2f%%" % [value]
 
 func _ready():
-		var hpRatio = GetRatio(GlobalWorld.currentPlayer.stat.health, GlobalWorld.currentPlayer.stat.maxHealth)
-		var manaRatio = GetRatio(GlobalWorld.currentPlayer.stat.mana, GlobalWorld.currentPlayer.stat.maxMana)
-		var staminaRatio = GetRatio(GlobalWorld.currentPlayer.stat.stamina, GlobalWorld.currentPlayer.stat.maxStamina)
+	assert(hpStat && manaStat && staminaStat, "Stat progress bars are missing")
 
-		var hpFormat = GetBarFormat(GlobalWorld.currentPlayer.stat.health, GlobalWorld.currentPlayer.stat.maxHealth)
-		var manaFormat = GetBarFormat(GlobalWorld.currentPlayer.stat.mana, GlobalWorld.currentPlayer.stat.maxMana)
-		var staminaFormat = GetBarFormat(GlobalWorld.currentPlayer.stat.stamina, GlobalWorld.currentPlayer.stat.maxStamina)
+	if hpStat:
+		hpStat.SetStat(GlobalWorld.currentPlayer.stat.health, GlobalWorld.currentPlayer.stat.maxHealth)
+	if manaStat:
+		manaStat.SetStat(GlobalWorld.currentPlayer.stat.mana, GlobalWorld.currentPlayer.stat.maxMana)
+	if staminaStat:
+		staminaStat.SetStat(GlobalWorld.currentPlayer.stat.stamina, GlobalWorld.currentPlayer.stat.maxStamina)
 
-		var levelFormat = String(GlobalWorld.currentPlayer.stat.level)
-		var expFormat = GetPercentFormat(GlobalWorld.currentPlayer.stat.experience)
+	var levelFormat = String(GlobalWorld.currentPlayer.stat.level)
+	LevelText.set_text(levelFormat)
 
-		HPBar.set_value(hpRatio)
-		HPText.set_text(hpFormat)
-
-		ManaBar.set_value(manaRatio)
-		ManaText.set_text(manaFormat)
-
-		StaminaBar.set_value(staminaRatio)
-		StaminaText.set_text(staminaFormat)
-		
-		LevelText.set_text(levelFormat)
-		ExpText.set_text(expFormat)
+	var expFormat = GetPercentFormat(GlobalWorld.currentPlayer.stat.experience)
+	ExpText.set_text(expFormat)
