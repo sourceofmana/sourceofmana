a16d0b4010365dc5dbc3d8df0c646e36d4e6e644
Reid <reidyaro@gmail.com>
2022-04-27 02:10:13 +0200

    Add temporal filling of progress bar

--- sources/gui/ProgressBar.gd
+++ sources/gui/ProgressBar.gd
@@ -1,19 +1,27 @@
 extends Control
 
-export(Texture)		var textureProgress
-export(Texture)		var textureBackground
-export(Color)		var labelColor
-export(float)		var labelScale
-export(String)		var labelUnit
+export(Texture)			var textureProgress
+export(Texture)			var textureBackground
+export(Color)			var labelColor
+export(String)			var labelUnit
+export(float)			var labelScale
+export(float)			var delayToFillSec
+export(int)				var precisionDivider
 
-onready var label	= get_node("Label")
-onready var bar		= get_node("Bar")
+onready var label		= get_node("Label")
+onready var bar			= get_node("Bar")
+
+var isUpdating			= false
+var remainsToFillSec	= 0.0
+var valueFrom			= 0
+var valueTo				= 0
+var valueMax			= 0
 
 #
-func GetRatio(currentValue : int, maxValue : int) -> float:
+func GetRatio(currentValue : float, maxValue : float) -> float:
 	var ratio = 0.0
 	if maxValue > 0:
-		ratio = float(currentValue) / maxValue * 100
+		ratio = currentValue / maxValue * 100.0
 	return ratio
 
 func GetFormatedText(value : String) -> String:
@@ -41,14 +49,47 @@ func GetBarFormat(currentValue : float, maxValue : float) -> String:
 	return formatedText
 
 #
-func SetStat(currentValue, maxValue):
+func SetStat(newValue, maxValue):
 	assert(bar && label, "ProgressBar childs are missing")
 
-	if bar:
-		bar.set_value(GetRatio(currentValue, maxValue))
-	if label:
-		label.set_text(GetBarFormat(currentValue, maxValue))
+	remainsToFillSec = delayToFillSec
+	isUpdating = true
+	valueFrom = bar.get_value()
+	valueTo = newValue
+	valueMax = maxValue
 
+	UpdateValue(0)
+
+func UpdateValue(delta):
+	if isUpdating:
+		remainsToFillSec -= delta
+
+		var ratioToFinish : float = 0
+		if delayToFillSec != 0:
+			ratioToFinish = (delayToFillSec - remainsToFillSec) / delayToFillSec
+			ratioToFinish = ease(ratioToFinish, 0.3)
+
+		var newValue = lerp(valueFrom, valueTo, ratioToFinish)
+		if bar:
+			bar.set_value(GetRatio(newValue, valueMax))
+
+		if precisionDivider != 0:
+			newValue = round(newValue * precisionDivider) / precisionDivider
+		else:
+			newValue = round(newValue)
+
+		if label:
+			label.set_text(GetBarFormat(newValue, valueMax))
+
+		if valueFrom == valueTo || remainsToFillSec <= 0:
+			if bar:
+				bar.set_value(GetRatio(valueTo, valueMax))
+			if label:
+				label.set_text(GetBarFormat(valueTo, valueMax))
+			
+			isUpdating = false
+			remainsToFillSec = 0
+			valueFrom = valueTo
 #
 func _ready():
 	assert(bar && label, "ProgressBar childs are missing")
@@ -66,5 +107,5 @@ func _ready():
 		if labelColor:
 			label.add_color_override("font_color", labelColor)
 
-func _process(_delta):
-	pass
+func _process(delta):
+	UpdateValue(delta)
