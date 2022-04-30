c249877eae19295593aa46f2a6062c5a3b5da93b
Reid <reidyaro@gmail.com>
2022-04-30 17:20:37 +0200

    Add DB service management on Launcher

--- sources/Launcher.gd
+++ sources/Launcher.gd
@@ -1,6 +1,12 @@
 extends Node
 
-var gameTitle = "Source of Mana v0.1"
+
+var gameTitle			= "Source of Mana v0.1"
+var DB					= load("res://sources/db/DB.gd").new()
 
 func _process(_delta):
 	OS.set_window_title(gameTitle + " | fps: " + str(Engine.get_frames_per_second()))
+
+func _ready():
+	# Load all services
+	DB.Init()
