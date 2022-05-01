b893afd8300d50873fdc4e5e2db6f8c05e0d5295
Reid <reidyaro@gmail.com>
2022-05-01 20:42:10 +0200

    Add a minimal window size

--- sources/Launcher.gd
+++ sources/Launcher.gd
@@ -1,11 +1,15 @@
 extends Node
 
-
+var minWindowSize		= Vector2(640, 480)
 var gameTitle			= "Source of Mana v0.1"
 var DB					= load("res://sources/db/DB.gd").new()
 
+#
 func _process(_delta):
-	OS.set_window_title(gameTitle + " | fps: " + str(Engine.get_frames_per_second()))
+	if OS.is_debug_build():
+		OS.set_window_title(gameTitle + " | fps: " + str(Engine.get_frames_per_second()))
+
+	OS.set_min_window_size(minWindowSize)
 
 func _ready():
 	# Load all services
