5391dd83ef1235ff0d3a79a925f764b1044b585d
Reid <reidyaro@gmail.com>
2022-05-10 01:36:49 +0200

    Add map db parsing

--- sources/db/DB.gd
+++ sources/db/DB.gd
@@ -4,6 +4,11 @@ const Item						= preload("res://sources/db/instance/Item.gd")
 var ItemsDBPath : String		= "res://data/db/items.json"
 var ItemsDB : Array				= []
 
+const Map						= preload("res://sources/db/instance/Map.gd")
+var MapsDBPath : String			= "res://data/db/maps.json"
+var MapsDB : Array				= []
+
+
 func Parse(path : String):
 	var result
 	var DBFile : File = File.new()
@@ -27,17 +32,29 @@ func Parse(path : String):
 	return result
 
 #
-func ParseItemDB():
+func ParseItemsDB():
 	var result = Parse(ItemsDBPath)
 	if result != null:
 		for key in result:
 			var item : Item = Item.new()
+			item._id = key
 			item._name = result[key].Name
 			item._description =  result[key].Description
-			item._path =  result[key].Path
-
+			item._path = result[key].Path
 			ItemsDB.append(item)
+
+func ParseMapsDB():
+	var result = Parse(MapsDBPath)
+	if result != null:
+		for key in result:
+			var map : Map = Map.new()
+			map._name = key
+			map._path = result[key].Path
+			MapsDB.append(map)
 
 #
 func Init():
-	ParseItemDB()
+	ParseItemsDB()
+	ParseMapsDB()
+	print(ItemsDB)
+	print(MapsDB)
