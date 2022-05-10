ee73b12ddecd754384bd00a2909b6a90b7d3fb0a
Reid <reidyaro@gmail.com>
2022-05-10 04:10:10 +0200

    Add Clint Bellanger's gold tiles

---
+++ data/graphics/tilesets/generic/cave/Gold.tsx
@@ -0,0 +1,59 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.1" name="Gold" tilewidth="32" tileheight="32" tilecount="16" columns="4">
+ <image source="gold.png" width="128" height="128"/>
+ <tile id="5">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="6" y="14" width="13" height="10"/>
+  </objectgroup>
+ </tile>
+ <tile id="6">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="3" y="16" width="17" height="13"/>
+  </objectgroup>
+ </tile>
+ <tile id="7">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="10" y="12" width="13" height="10"/>
+  </objectgroup>
+ </tile>
+ <tile id="8">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="4" y="14" width="17" height="15"/>
+  </objectgroup>
+ </tile>
+ <tile id="9">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="16" y="13" width="16" height="19"/>
+  </objectgroup>
+ </tile>
+ <tile id="10">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="10" width="26" height="22"/>
+  </objectgroup>
+ </tile>
+ <tile id="11">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="7" y="5" width="24" height="26"/>
+  </objectgroup>
+ </tile>
+ <tile id="12">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="2" y="12" width="21" height="13"/>
+  </objectgroup>
+ </tile>
+ <tile id="13">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="2" y="5" width="29" height="26"/>
+  </objectgroup>
+ </tile>
+ <tile id="14">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="6" y="7" width="23" height="24"/>
+  </objectgroup>
+ </tile>
+ <tile id="15">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="8" y="0" width="24" height="32"/>
+  </objectgroup>
+ </tile>
+</tileset>
