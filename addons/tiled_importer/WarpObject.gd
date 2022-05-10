85cfe37af2c8a09f19c9f9416e322d185ba764dc
Reid <reidyaro@gmail.com>
2022-05-10 03:11:18 +0200

    Un-hardcode map warp, use map db to warp the player

--- addons/tiled_importer/WarpObject.gd
+++ addons/tiled_importer/WarpObject.gd
@@ -7,7 +7,10 @@ export var destinationPos : Vector2			= Vector2.ZERO
 #
 func Warp(body):
 	if body is KinematicBody2D:
-		GlobalWorld.SetPlayerInWorld("res://data/maps/phatina/" + destinationMap + ".tmx", destinationPos)
+		if destinationMap.empty() == false:
+			var mapReference = Launcher.DB.MapsDB[destinationMap]
+			if mapReference:
+				GlobalWorld.SetPlayerInWorld(Launcher.Path.MapRsc + mapReference._path, destinationPos)
 
 #
 func _ready():
