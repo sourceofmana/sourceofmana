b0cc2064dd7c5d6f06f925f0380a17b848936e77
Reid <reidyaro@gmail.com>
2022-05-15 19:31:25 +0200

    Add FileSystem service

---
+++ sources/system/FileSystem.gd
@@ -0,0 +1,52 @@
+extends Node
+
+onready var directory = Directory.new();
+
+# Generic
+func Exists(path : String) -> bool:
+	return directory.file_exists(path)
+
+# Source
+func LoadSource(path : String) -> Node:
+	var fullPath : String		= Launcher.Path.Src + path
+	var srcFile : Node			= null
+
+	var pathExists : bool		= Exists(fullPath)
+	assert(pathExists, "Source file not found " + path + " should be located at " + fullPath)
+
+	if pathExists:
+		srcFile = load(fullPath).new()
+
+	return srcFile
+
+# Config
+func LoadConfig(path : String) -> ConfigFile:
+	var fullPath : String		= Launcher.Path.Config + path
+	var cfgFile : ConfigFile	= null
+
+	var pathExists : bool		= Exists(fullPath)
+	assert(pathExists, "Config file not found " + path + " should be located at " + fullPath)
+
+	if pathExists:
+		cfgFile = ConfigFile.new()
+
+		var err = cfgFile.load(fullPath)
+		assert(err == OK, "Error loading the config file " + path + " located at " + fullPath)
+
+		if err != OK:
+			cfgFile.free()
+			cfgFile = null
+
+	return cfgFile
+
+func SaveConfig(path : String, cfgFile : ConfigFile):
+	var fullPath = Launcher.Path.Config + path
+	assert(cfgFile, "Config file " + path + " not initialized")
+
+	if cfgFile:
+		var pathExists : bool = Exists(fullPath)
+		assert(pathExists, "Config file not found " + path + " should be located at " + fullPath)
+
+		if pathExists:
+			var err = cfgFile.save(fullPath)
+			assert(err == OK, "Error saving the config file " + path + " located at " + fullPath)
