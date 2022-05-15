5b606e58b704e17cef2ff0e5179f2e3eabef02b7
Reid <reidyaro@gmail.com>
2022-05-16 00:28:24 +0200

    Add conf and scenes global paths

--- sources/system/Path.gd
+++ sources/system/Path.gd
@@ -8,6 +8,11 @@ const ItemRsc : String					= GfxRsc + "items/"
 const MapRsc : String					= DataRsc + "maps/"
 const MusicRsc : String					= DataRsc + "musics/"
 const DBRsc : String					= DataRsc + "db/"
+const ConfRsc : String					= DataRsc + "conf/"
 
 const Src : String						= Rsc + "sources/"
 const DBInstSrc : String				= Src + "db/instance/"
+
+const Scn : String						= Rsc + "scenes/"
+const GuiScn : String					= Scn + "gui/"
+const PresetScn : String				= Scn + "presets/"
