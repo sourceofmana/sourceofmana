72bd83bde85ae8f4786c7a12de66e52dfea51e09
Reid <reidyaro@gmail.com>
2022-08-15 00:29:06 +0200

    Prevent a crash if no player found when initializing the game GUI

--- sources/gui/GameGui.gd
+++ sources/gui/GameGui.gd
@@ -5,8 +5,8 @@ onready var manaStat		= $VBoxMain/HBoxTop/StatIndicator/Bars/Mana
 onready var staminaStat		= $VBoxMain/HBoxTop/StatIndicator/Bars/Stamina
 onready var weightStat		= $FloatingWindows/Inventory/VBoxContainer/Weight/BgTex/Weight
 
-onready var LevelText		= $VBoxMain/HBoxTop/StatIndicator/LevelText
-onready var ExpText			= $VBoxMain/HBoxTop/StatIndicator/ExpText
+onready var levelText		= $VBoxMain/HBoxTop/StatIndicator/LevelText
+onready var expText			= $VBoxMain/HBoxTop/StatIndicator/ExpText
 
 #
 func GetPercentFormat(value : int) -> String:
@@ -14,17 +14,20 @@ func GetPercentFormat(value : int) -> String:
 
 #
 func _ready():
-	assert(hpStat && manaStat && staminaStat, "Stat progress bars are missing")
-	assert(weightStat, "Stat inventory weight bar is missing")
+	if Launcher.Entities.activePlayer:
+		assert(hpStat && manaStat && staminaStat, "Stat progress bars are missing")
+		assert(weightStat, "Stat inventory weight bar is missing")
 
-	if hpStat:
-		hpStat.SetStat(Launcher.Entities.activePlayer.stat.health, Launcher.Entities.activePlayer.stat.maxHealth)
-	if manaStat:
-		manaStat.SetStat(Launcher.Entities.activePlayer.stat.mana, Launcher.Entities.activePlayer.stat.maxMana)
-	if staminaStat:
-		staminaStat.SetStat(Launcher.Entities.activePlayer.stat.stamina, Launcher.Entities.activePlayer.stat.maxStamina)
-	if weightStat:
-		weightStat.SetStat(Launcher.Entities.activePlayer.stat.weight, Launcher.Entities.activePlayer.stat.maxWeight)
+		if hpStat:
+			hpStat.SetStat(Launcher.Entities.activePlayer.stat.health, Launcher.Entities.activePlayer.stat.maxHealth)
+		if manaStat:
+			manaStat.SetStat(Launcher.Entities.activePlayer.stat.mana, Launcher.Entities.activePlayer.stat.maxMana)
+		if staminaStat:
+			staminaStat.SetStat(Launcher.Entities.activePlayer.stat.stamina, Launcher.Entities.activePlayer.stat.maxStamina)
+		if weightStat:
+			weightStat.SetStat(Launcher.Entities.activePlayer.stat.weight, Launcher.Entities.activePlayer.stat.maxWeight)
 
-	LevelText.set_text(String(Launcher.Entities.activePlayer.stat.level))
-	ExpText.set_text(GetPercentFormat(Launcher.Entities.activePlayer.stat.experience))
+		if levelText:
+			levelText.set_text(String(Launcher.Entities.activePlayer.stat.level))
+		if expText:
+			expText.set_text(GetPercentFormat(Launcher.Entities.activePlayer.stat.experience))
