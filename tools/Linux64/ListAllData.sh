f13b01607a340a178f3c31b5667e81d3812ce9ed
Reid <reidyaro@gmail.com>
2022-08-22 01:46:04 +0200

    Add missing map files on the license file

--- tools/Linux64/ListAllData.sh
+++ tools/Linux64/ListAllData.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd ../../data/
-find . -type f -name "*.png" -o -name "*.svg" -o -name "*.ttf" -o -name "*.ogg"
+find . -type f -name "*.png" -o -name "*.svg" -o -name "*.ttf" -o -name "*.ogg" -o -name "*.tmx"
