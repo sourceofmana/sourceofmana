1b9e736314a0c69e0c029b9bc5cd5c224213845d
Reid <reidyaro@gmail.com>
2023-01-24 00:44:37 +0100

    Rewrite the spawn logic

--- sources/world/World.gd
+++ sources/world/World.gd
@@ -20,7 +20,7 @@ class Map:
 var areas : Dictionary = {}
 
 # Utils
-func GenerateRandomPosition(map : Map) -> Vector2:
+func GetRandomPosition(map : Map) -> Vector2i:
 	Launcher.Util.Assert(map != null && map.nav_poly != null && map.nav_poly.get_polygon_count() > 0, "No triangulation available")
 	if map != null && map.nav_poly != null && map.nav_poly.get_polygon_count() > 0:
 		var outlinesList : PackedVector2Array  = map.nav_poly.get_vertices()
@@ -33,9 +33,23 @@ func GenerateRandomPosition(map : Map) -> Vector2:
 		var b : Vector2 = outlinesList[randPolygon[(randVerticeID + 1) % randPolygon.size()]]
 		var c : Vector2 = outlinesList[randPolygon[(randVerticeID + 2) % randPolygon.size()]]
 
-		return a + sqrt(randf()) * (-a + b + randf() * (c - b))
+		return Vector2i(a + sqrt(randf()) * (-a + b + randf() * (c - b)))
 
-	return Vector2.ZERO
+	Launcher.Util.Assert(false, "Mob could not be spawned, no available point on the navigation mesh were found")
+	return Vector2i.ZERO
+
+func GetRandomPositionAABB(map : Map, pos : Vector2i, offset : Vector2i) -> Vector2i:
+	Launcher.Util.Assert(map != null, "Could not create a random position for a non-initialized map")
+	if map != null:
+		for i in Launcher.Conf.GetInt("Navigation", "navigationSpawnTry", Launcher.Conf.Type.SERVER):
+			var randPoint : Vector2i = Vector2i(randi_range(0, offset.x), randi_range(0, offset.y))
+			randPoint += pos
+
+			var closestPoint : Vector2i = NavigationServer2D.map_get_closest_point(map.mapRID, randPoint)
+			if randPoint == closestPoint:
+				return randPoint
+
+	return GetRandomPosition(map)
 
 # Instance init
 func ParseInformation(map : Map):
@@ -44,9 +58,25 @@ func ParseInformation(map : Map):
 		if "nav_poly" in node:
 			map.nav_poly = node.nav_poly
 		if "spawns" in node:
-			map.spawns = node.spawns
+			for spawn in node.spawns:
+				Launcher.Util.Assert(spawn != null, "Warp format is not supported")
+				if spawn:
+					var spawnObject = SpawnObject.new()
+					spawnObject.mob_count = spawn[0]
+					spawnObject.mob_name = spawn[1]
+					spawnObject.spawn_position = spawn[2]
+					spawnObject.spawn_offset = spawn[3]
+					spawnObject.is_global = spawnObject.spawn_position < Vector2i.LEFT
+					map.spawns.append(spawnObject)
 		if "warps" in node:
-			map.warps = node.warps
+			for warp in node.warps:
+				Launcher.Util.Assert(warp != null, "Warp format is not supported")
+				if warp:
+					var warpObject = WarpObject.new()
+					warpObject.destinationMap = warp[0]
+					warpObject.destinationPos = warp[1]
+					warpObject.polygon = warp[2]
+					map.warps.append(warpObject)
 
 		map.mapRID = NavigationServer2D.map_create()
 		NavigationServer2D.map_set_active(map.mapRID, true)
@@ -55,25 +85,28 @@ func ParseInformation(map : Map):
 		NavigationServer2D.region_set_map(map.regionRID, map.mapRID)
 		NavigationServer2D.region_set_navigation_polygon(map.regionRID, map.nav_poly)
 
+		NavigationServer2D.map_force_update(map.mapRID)
+
 		node.queue_free()
 
 func CreateInstance(map : Map, instanceID : int = 0):
 	var inst : Instance = Instance.new()
 
 	inst.id = instanceID
-	inst.npcs.append(CreateNpc("Becees"))
-	inst.mobs.append(CreateMob("Phatyna"))
-	inst.mobs.append(CreateMob("Dorian"))
-	inst.mobs.append(CreateMob("Emil"))
-	inst.mobs.append(CreateMob("Gabriel"))
-	inst.mobs.append(CreateMob("Lilah"))
-	inst.mobs.append(CreateMob("Lulea"))
-	inst.mobs.append(CreateMob("Marvin"))
+	for spawn in map.spawns:
+		for i in spawn.mob_count:
+			var mob : BaseEntity = CreateMob(spawn.mob_name)
+			if spawn.is_global:
+				mob.position = GetRandomPosition(map)
+			else:
+				mob.position = GetRandomPositionAABB(map, spawn.spawn_position, spawn.spawn_offset)
+			inst.mobs.append(mob)
 
 	map.instances.push_back(inst)
 
 	for entity in inst.npcs + inst.players + inst.mobs:
-		entity.position = GenerateRandomPosition(map)
+		if entity.position == Vector2.ZERO:
+			entity.position = GetRandomPosition(map)
 		if entity.agent:
 			entity.agent.set_navigation_map(map.mapRID)
 
@@ -168,7 +201,7 @@ func CreateNpc(entityID : String, entityName : String = "") -> BaseEntity:
 
 # AI
 func UpdateWalkPaths(entity : Node2D, map : Map):
-	var newPos : Vector2 = GenerateRandomPosition(map)
+	var newPos : Vector2 = GetRandomPosition(map)
 	entity.WalkToward(newPos)
 
 func UpdateAI(entity : BaseEntity, map : Map):
