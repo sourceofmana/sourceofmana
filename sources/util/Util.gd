6e250e1ede93e93a7089cd1f06ad3eccb5892b08
Reid <reidyaro@gmail.com>
2023-03-19 14:46:41 +0100

    Add some new callback utils

--- sources/util/Util.gd
+++ sources/util/Util.gd
@@ -8,3 +8,20 @@ func Assert(condition : bool, message : String) -> void:
 
 func PrintLog(logGroup : String, logString : String):
 	print("[%d.%03d][%s] %s" % [Time.get_ticks_msec() / 1000.0, Time.get_ticks_msec() % 1000, logGroup, logString])
+
+#
+func ReplaceCallback(objectSignal : Signal, callback : Callable, args : Array):
+	if objectSignal.is_connected(callback):
+		objectSignal.disconnect(callback)
+
+	var callable : Callable = callback.bind(args) if callback == ShootCallback else callback.bindv(args)
+	objectSignal.connect(callable)
+
+func OneShotCallback(objectSignal : Signal, callback : Callable, args : Array):
+	ReplaceCallback(objectSignal, ShootCallback, [callback] + args)
+
+func ShootCallback(args : Array):
+	if args.size() > 0:
+		var callback : Callable = args.pop_front()
+		if callback:
+			callback.callv(args)
