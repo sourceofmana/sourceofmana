f7a01baa338e9798dc6983d8580679f36a27bfff
Reid <reidyaro@gmail.com>
2023-04-02 04:27:45 +0200

    Add a default offset value on our torch glow script

--- sources/effects/TorchGlow.gd
+++ sources/effects/TorchGlow.gd
@@ -5,6 +5,7 @@ var generalSpeed : float				= 10
 var positionSpeed : float				= 2
 var baseEnergy : float					= 0.9
 var baseScale : float					= 0.9
+var baseOffset : Vector2				= Vector2.ZERO
 var modulateEnergy : float				= 0.1
 var modulateScale : float				= 0.1
 var maxOffset : float					= 2
@@ -16,7 +17,7 @@ func _physics_process(delta):
 
 	energy			= baseEnergy + modulateEnergy * sinValue
 	texture_scale	= baseScale + modulateScale * sinValue
-	offset			= Vector2(sin(value * positionSpeed), cos(value * positionSpeed)) * maxOffset
+	offset			= baseOffset + Vector2(sin(value * positionSpeed), cos(value * positionSpeed)) * maxOffset
 
 func _ready():
 	randomize()
@@ -24,6 +25,7 @@ func _ready():
 
 	baseEnergy	= energy * 0.97
 	baseScale	= texture_scale * 0.99
+	baseOffset	= offset
 
 	modulateEnergy	= energy - baseEnergy
 	modulateScale	= texture_scale - baseScale
