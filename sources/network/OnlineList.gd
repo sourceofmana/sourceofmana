ce81d812afcdc57edae72b607a45e99a35a40ca7
LawnCable <git@lawncable.net>
2023-04-24 03:01:23 +0200

    only generate json now and add a js based site

--- sources/network/OnlineList.gd
+++ sources/network/OnlineList.gd
@@ -1,7 +1,6 @@
 extends Object
 class_name OnlineList
 
-var HTMLFileName : String				= "index.html"
 var JSONFileName : String				= "online.json"
 
 #
@@ -14,35 +13,6 @@ func GetPlayerNames() -> Array[String]:
 				players.append(agent.agentName)
 	return players
 
-func UpdateHtmlPageAndJson():
+func UpdateJson():
 	var players : Array[String]	= GetPlayerNames()
-	var content : String		= ""
-
-	content += "<HTML>\n"
-	content += "  <META http-equiv=\"Refresh\" content=\"%d\">\n" % 20
-	content += "  <HEAD>\n"
-	content += "    <TITLE>Online Players on %s</TITLE>\n" % "Source of Mana"
-	content += "  </HEAD>\n"
-	content += "  <BODY>\n"
-	content += "    <H3>Online Players on %s (%s):</H3>\n" % ["Source of Mana", Time.get_datetime_string_from_system(true, true)]
-
-	var playerCount : int = players.size()
-	if playerCount > 0:
-		content += "    <table border=\"1\" cellspacing=\"1\">\n"
-		content += "      <tr>\n"
-		content += "        <th>Name</th>\n"
-		content += "      </tr>\n"
-
-		for player in players:
-			content += "      <tr>\n"
-			content += "        <td>%s</th>\n" % str(player)
-			content += "      </tr>\n"
-
-	content += "    </table>\n"
-	content += "    <p>%d users are online.</p>\n" % playerCount
-	content += "    <p>If you look for a machine readable online list: <a href=\"./online.json\">online.json</a></p>\n"
-	content += "  </BODY>\n"
-	content += "</HTML>\n"
-
-	Launcher.FileSystem.SaveFile(OS.get_executable_path().get_base_dir() + "/" + HTMLFileName, content)
 	Launcher.FileSystem.SaveFile(OS.get_executable_path().get_base_dir() + "/" + JSONFileName, JSON.stringify(players))
