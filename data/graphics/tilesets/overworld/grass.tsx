c01360f4a242eeb0caf050755fbb20018138d391
Reid <reidyaro@gmail.com>
2023-06-03 00:32:34 +0200

    Rework tile collision shapes for the overworld map

--- data/graphics/tilesets/overworld/grass.tsx
+++ data/graphics/tilesets/overworld/grass.tsx
@@ -1,239 +1,95 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="Grass" tilewidth="16" tileheight="16" tilecount="252" columns="12">
- <image source="grass.png" width="192" height="336"/>
- <tile id="0" probability="20"/>
+<tileset version="1.9" tiledversion="1.9.2" name="Grass" tilewidth="16" tileheight="16" tilecount="144" columns="12">
+ <image source="grass.png" width="192" height="192"/>
  <tile id="48">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="0">
-    <polygon points="0,0 16,0 16,16 0,16"/>
+    <polyline points="0,0 16,0 16,16 0,16 0,0"/>
    </object>
   </objectgroup>
  </tile>
  <tile id="49">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="0">
-    <polygon points="0,0 16,0 16,16 0,16"/>
+    <polyline points="0,0 16,0 16,16 0,16 0,0"/>
    </object>
   </objectgroup>
  </tile>
  <tile id="50">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="0">
-    <polygon points="0,0 16,0 16,16 0,16"/>
+    <polyline points="0,0 16,0 16,16 0,16 0,0"/>
    </object>
   </objectgroup>
  </tile>
  <tile id="60">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="0">
-    <polygon points="0,0 16,0 16,16 0,16"/>
+    <polyline points="0,0 16,0 16,16 0,16 0,0"/>
    </object>
   </objectgroup>
  </tile>
  <tile id="61">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="0">
-    <polygon points="0,0 16,0 16,16 0,16"/>
+    <polyline points="0,0 16,0 16,16 0,16 0,0"/>
    </object>
   </objectgroup>
  </tile>
  <tile id="62">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="0">
-    <polygon points="0,0 16,0 16,16 0,16"/>
+    <polyline points="0,0 16,0 16,16 0,16 0,0"/>
    </object>
   </objectgroup>
  </tile>
  <tile id="72">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="0">
-    <polygon points="0,0 16,0 16,16 0,16"/>
+    <polyline points="0,0 16,0 16,16 0,16 0,0"/>
    </object>
   </objectgroup>
  </tile>
  <tile id="73">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="0">
-    <polygon points="0,0 16,0 16,16 0,16"/>
+    <polyline points="0,0 16,0 16,16 0,16 0,0"/>
    </object>
   </objectgroup>
  </tile>
  <tile id="74">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="0">
-    <polygon points="0,0 16,0 16,16 0,16"/>
+    <polyline points="0,0 16,0 16,16 0,16 0,0"/>
    </object>
   </objectgroup>
  </tile>
  <tile id="84">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="0">
-    <polygon points="0,0 16,0 16,16 0,16"/>
+    <polyline points="0,0 16,0 16,16 0,16 0,0"/>
    </object>
   </objectgroup>
  </tile>
  <tile id="85">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="0">
-    <polygon points="0,0 16,0 16,16 0,16"/>
+    <polyline points="0,0 16,0 16,16 0,16 0,0"/>
    </object>
   </objectgroup>
  </tile>
  <tile id="96">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="0">
-    <polygon points="0,0 16,0 16,16 0,16"/>
+    <polyline points="0,0 16,0 16,16 0,16 0,0"/>
    </object>
   </objectgroup>
  </tile>
  <tile id="97">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="0">
-    <polygon points="0,0 16,0 16,16 0,16"/>
+    <polyline points="0,0 16,0 16,16 0,16 0,0"/>
    </object>
   </objectgroup>
  </tile>
- <tile id="144">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="16">
-    <polyline points="0,0 16,-7 16,0 0,0"/>
-   </object>
-  </objectgroup>
- </tile>
- <tile id="145">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="16">
-    <polyline points="0,0 0,-7 16,-7 16,0 0,0"/>
-   </object>
-  </objectgroup>
- </tile>
- <tile id="146">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="16">
-    <polyline points="0,0 0,-7 16,-7 16,0 0,0"/>
-   </object>
-  </objectgroup>
- </tile>
- <tile id="147">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="16">
-    <polyline points="0,0 0,-7 16,0 0,0"/>
-   </object>
-  </objectgroup>
- </tile>
- <tile id="156">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="0">
-    <polygon points="0,0 16,0 6,16 0,16"/>
-   </object>
-  </objectgroup>
- </tile>
- <tile id="159">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="0">
-    <polygon points="0,0 16,0 16,16 12,16"/>
-   </object>
-  </objectgroup>
- </tile>
- <tile id="168">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="0">
-    <polygon points="0,0 7,0 16,16 0,16"/>
-   </object>
-  </objectgroup>
- </tile>
- <tile id="171">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="12" y="0">
-    <polygon points="0,0 -12,16 4,16 4,0"/>
-   </object>
-  </objectgroup>
- </tile>
- <tile id="180">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="0">
-    <polygon points="0,0 16,0 16,16 0,16"/>
-   </object>
-  </objectgroup>
- </tile>
- <tile id="181">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="0">
-    <polygon points="0,0 16,0 16,16 0,16"/>
-   </object>
-  </objectgroup>
- </tile>
- <tile id="183">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="0">
-    <polygon points="0,0 16,0 16,16 0,16"/>
-   </object>
-  </objectgroup>
- </tile>
- <wangsets>
-  <wangset name="Terrains" type="corner" tile="-1">
-   <wangcolor name="Grass" color="#ff0000" tile="13" probability="1"/>
-   <wangcolor name="Water" color="#00ff00" tile="0" probability="1"/>
-   <wangcolor name="Forest" color="#0000ff" tile="0" probability="1"/>
-   <wangcolor name="Swampgrass" color="#ff7700" tile="0" probability="1"/>
-   <wangcolor name="Swamp" color="#00e9ff" tile="0" probability="1"/>
-   <wangtile tileid="0" wangid="0,1,0,1,0,1,0,1"/>
-   <wangtile tileid="1" wangid="0,1,0,1,0,1,0,1"/>
-   <wangtile tileid="2" wangid="0,1,0,1,0,1,0,1"/>
-   <wangtile tileid="3" wangid="0,1,0,1,0,1,0,1"/>
-   <wangtile tileid="4" wangid="0,1,0,1,0,1,0,1"/>
-   <wangtile tileid="13" wangid="0,1,0,1,0,1,0,1"/>
-   <wangtile tileid="14" wangid="0,1,0,1,0,1,0,1"/>
-   <wangtile tileid="25" wangid="0,1,0,1,0,1,0,1"/>
-   <wangtile tileid="26" wangid="0,1,0,1,0,1,0,1"/>
-   <wangtile tileid="37" wangid="0,1,0,1,0,1,0,1"/>
-   <wangtile tileid="38" wangid="0,1,0,1,0,1,0,1"/>
-   <wangtile tileid="48" wangid="0,1,0,2,0,1,0,1"/>
-   <wangtile tileid="49" wangid="0,1,0,2,0,2,0,1"/>
-   <wangtile tileid="50" wangid="0,1,0,1,0,2,0,1"/>
-   <wangtile tileid="51" wangid="0,4,0,5,0,4,0,4"/>
-   <wangtile tileid="52" wangid="0,4,0,5,0,5,0,4"/>
-   <wangtile tileid="53" wangid="0,4,0,4,0,5,0,4"/>
-   <wangtile tileid="57" wangid="0,1,0,3,0,1,0,1"/>
-   <wangtile tileid="58" wangid="0,1,0,3,0,3,0,1"/>
-   <wangtile tileid="59" wangid="0,1,0,1,0,3,0,1"/>
-   <wangtile tileid="60" wangid="0,2,0,2,0,1,0,1"/>
-   <wangtile tileid="61" wangid="0,2,0,2,0,2,0,2"/>
-   <wangtile tileid="62" wangid="0,1,0,1,0,2,0,2"/>
-   <wangtile tileid="63" wangid="0,5,0,5,0,4,0,4"/>
-   <wangtile tileid="64" wangid="0,5,0,5,0,5,0,5"/>
-   <wangtile tileid="65" wangid="0,4,0,4,0,5,0,5"/>
-   <wangtile tileid="69" wangid="0,3,0,3,0,1,0,1"/>
-   <wangtile tileid="70" wangid="0,3,0,3,0,3,0,3"/>
-   <wangtile tileid="71" wangid="0,1,0,1,0,3,0,3"/>
-   <wangtile tileid="72" wangid="0,2,0,1,0,1,0,1"/>
-   <wangtile tileid="73" wangid="0,2,0,1,0,1,0,2"/>
-   <wangtile tileid="74" wangid="0,1,0,1,0,1,0,2"/>
-   <wangtile tileid="75" wangid="0,5,0,4,0,4,0,4"/>
-   <wangtile tileid="76" wangid="0,5,0,4,0,4,0,5"/>
-   <wangtile tileid="77" wangid="0,4,0,4,0,4,0,5"/>
-   <wangtile tileid="81" wangid="0,3,0,1,0,1,0,1"/>
-   <wangtile tileid="82" wangid="0,3,0,1,0,1,0,3"/>
-   <wangtile tileid="83" wangid="0,1,0,1,0,1,0,3"/>
-   <wangtile tileid="84" wangid="0,2,0,1,0,2,0,2"/>
-   <wangtile tileid="85" wangid="0,2,0,2,0,1,0,2"/>
-   <wangtile tileid="87" wangid="0,5,0,4,0,5,0,5"/>
-   <wangtile tileid="88" wangid="0,5,0,5,0,4,0,5"/>
-   <wangtile tileid="89" wangid="0,4,0,4,0,4,0,4"/>
-   <wangtile tileid="93" wangid="0,3,0,1,0,3,0,3"/>
-   <wangtile tileid="94" wangid="0,3,0,3,0,1,0,3"/>
-   <wangtile tileid="96" wangid="0,1,0,2,0,2,0,2"/>
-   <wangtile tileid="97" wangid="0,2,0,2,0,2,0,1"/>
-   <wangtile tileid="99" wangid="0,4,0,5,0,5,0,5"/>
-   <wangtile tileid="100" wangid="0,5,0,5,0,5,0,4"/>
-   <wangtile tileid="105" wangid="0,1,0,3,0,3,0,3"/>
-   <wangtile tileid="106" wangid="0,3,0,3,0,3,0,1"/>
-   <wangtile tileid="123" wangid="0,4,0,4,0,4,0,4"/>
-   <wangtile tileid="124" wangid="0,4,0,4,0,4,0,4"/>
-   <wangtile tileid="125" wangid="0,4,0,4,0,4,0,4"/>
-   <wangtile tileid="135" wangid="0,4,0,4,0,4,0,4"/>
-   <wangtile tileid="136" wangid="0,4,0,4,0,4,0,4"/>
-   <wangtile tileid="137" wangid="0,4,0,4,0,4,0,4"/>
-  </wangset>
- </wangsets>
 </tileset>
---
+++ data/graphics/tilesets/overworld/house.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="House" tilewidth="16" tileheight="16" tilecount="90" columns="10">
+ <image source="houses.png" width="160" height="144"/>
+</tileset>
