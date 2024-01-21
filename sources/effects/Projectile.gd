219f3a92905053b140e7d64a9ad31b3e48b289ca
Reid <reidyaro@gmail.com>
2024-01-21 13:44:46 +0100

    Fix a possible crash when the projectile callback is not valide anymore (i.e.: target is dead)

--- sources/effects/Projectile.gd
+++ sources/effects/Projectile.gd
@@ -12,7 +12,7 @@ var callable : Callable
 func _process(delta):
 	elapsed = min(delay, elapsed + delta)
 	if elapsed == delay:
-		if not callable.is_null():
+		if not callable.is_null() and callable.is_valid():
 			callable.call()
 		Util.RemoveNode(self, get_parent())
 	else:
