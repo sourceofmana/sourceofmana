48a4fd88a823262d09aee9710d72793519422b81
Reid <reidyaro@gmail.com>
2024-02-12 20:05:48 +0100

    Add two kind of theme variation to handle our button and key labels

--- sources/gui/Tips.gd
+++ sources/gui/Tips.gd
@@ -1,13 +1,39 @@
 extends PanelContainer
 
-@export var fadeInDelay : float		= 1.0
-@export var displayDelay : float	= 6.0
-@export var fadeOutDelay : float	= 8.0
+@export var fadeInDelay : float				= 1.0
+@export var displayDelay : float			= 6.0
+@export var fadeOutDelay : float			= 8.0
+@export var actions : Array[StringName]		= []
 
-var currentDelay : float			= 0.0
+@onready var tips : Control					= $MarginContainer/TipsList
+@onready var tipNode : PackedScene			= preload("res://presets/gui/TipNode.tscn")
+
+var currentDelay : float					= 0.0
 
 #
 func initialize():
+	for child in tips.get_children():
+		tips.remove_child(child)
+
+	for action in actions:
+		if InputMap.action_get_events(action).size() > 0:
+			var actionInfo : Array = DeviceManager.GetActionInfo(action)
+			if actionInfo.size() == DeviceManager.ActionInfo.COUNT:
+				var tip : Control = tipNode.instantiate()
+				var tipIcon : Label = tip.get_node("Icon")
+				var tipLabel : Label = tip.get_node("Label")
+				if tip and tipIcon and tipLabel:
+					match actionInfo[DeviceManager.ActionInfo.DEVICE_TYPE]:
+						DeviceManager.DeviceType.KEYBOARD:
+							tipIcon.set_theme_type_variation("KeyTip")
+						DeviceManager.DeviceType.JOYSTICK:
+							tipIcon.set_theme_type_variation("ButtonTip")
+						_:
+							Util.Assert(false, "Device Type not recognized")
+					tipIcon.set_text(actionInfo[DeviceManager.ActionInfo.NAME])
+					tipLabel.set_text(DeviceManager.GetActionName(action))
+					tips.add_child.call_deferred(tip)
+
 	currentDelay = 0.0
 	visible = true
 
