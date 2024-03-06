bf22e5f74c6b5e046789ef9bc788acaf472b14d1
Reid <reidyaro@gmail.com>
2024-03-06 01:21:28 +0100

    Use TouchScreenButton to manage our UI overlay to enable multi-touch support on mobile/tablet

--- sources/gui/Sticks.gd
+++ sources/gui/Sticks.gd
@@ -17,22 +17,6 @@ func GetMove() -> Vector2:
 		return Vector2.ZERO
 
 #
-func _on_button_y_pressed():
-	if Launcher.Network:
-		Launcher.Network.TriggerSit()
-
-func _on_button_b_pressed():
-	if Launcher.Network:
-		Launcher.Network.TriggerEmote(3)
-
-func _on_button_x_pressed():
-	if Launcher.GUI:
-		Launcher.Network.TriggerMorph()
-
-func _on_button_a_pressed():
-	if Launcher.Player:
-		Launcher.Player.Interact()
-
 func _on_direction_button_down():
 	leftStickPressed = true
 
@@ -42,17 +26,8 @@ func _on_direction_button_up():
 
 func _physics_process(_delta):
 	if leftStickPressed:
-#		DirectionButton.global_position = get_global_mouse_position() - DirectionButton.get_size() / 2
-
 		var newPos : Vector2 = get_local_mouse_position() - DirectionButton.get_size() / 2
 		newPos.x = clampf(newPos.x, 0.0, 100.0)
 		newPos.y = clampf(newPos.y, 0.0, 100.0)
 
 		DirectionButton.position = newPos
-
-
-#		newPos = clampPos + DirectionButton.get_global_position()
-
-#		DirectionButton.set_global_position(newPos)
-
-
