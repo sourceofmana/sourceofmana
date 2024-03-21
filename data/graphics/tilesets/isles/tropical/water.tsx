9012d85f10d5991f4a39b31cc4c07b074505218b
Reid <reidyaro@gmail.com>
2024-03-21 01:14:56 +0100

    Unify some palettes, remove some duplicated tiles and clean up the water tileset for drazil

--- data/graphics/tilesets/isles/tropical/water.tsx
+++ data/graphics/tilesets/isles/tropical/water.tsx
@@ -1,11 +1,34 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="Water" tilewidth="32" tileheight="32" tilecount="240" columns="15">
- <image source="water.png" width="480" height="512"/>
- <tile id="136">
+<tileset version="1.10" tiledversion="1.10.2" name="Water" tilewidth="32" tileheight="32" tilecount="90" columns="15">
+ <image source="water.png" width="480" height="192"/>
+ <tile id="0">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="0">
-    <polygon points="0,0 32,0 32,32 0,32"/>
-   </object>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="15">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="16">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="17">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
   </objectgroup>
  </tile>
 </tileset>
