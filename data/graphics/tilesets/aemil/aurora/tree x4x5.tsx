4dbc279c4a090d2f4982c520ee246cfc0ec79a9f
Reid <reidyaro@gmail.com>
2024-05-27 01:38:24 +0200

    Add falling leaf particle effect

--- data/graphics/tilesets/aemil/aurora/tree x4x5.tsx
+++ data/graphics/tilesets/aemil/aurora/tree x4x5.tsx
@@ -1,4 +1,12 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.2" name="Tree x4x5" tilewidth="128" tileheight="160" tilecount="4" columns="4">
+<tileset version="1.10" tiledversion="1.10.1" name="Tree x4x5" tilewidth="128" tileheight="160" tilecount="4" columns="4">
  <image source="tree-x4x5.png" width="512" height="160"/>
+ <tile id="0">
+  <properties>
+   <property name="FX" value="Leaf"/>
+   <property name="custom" value="FX"/>
+   <property name="offset_x" type="int" value="48"/>
+   <property name="offset_y" type="int" value="-64"/>
+  </properties>
+ </tile>
 </tileset>
--- data/graphics/tilesets/isles/tropical/Tree x3x4.tsx
+++ data/graphics/tilesets/isles/tropical/Tree x3x4.tsx
@@ -1,4 +1,12 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="Tree x3x4" tilewidth="96" tileheight="128" tilecount="10" columns="5">
+<tileset version="1.10" tiledversion="1.10.1" name="Tree x3x4" tilewidth="96" tileheight="128" tilecount="10" columns="5">
  <image source="tree-x3x4.png" width="480" height="256"/>
+ <tile id="6">
+  <properties>
+   <property name="FX" value="Leaf"/>
+   <property name="custom" value="FX"/>
+   <property name="offset_x" type="int" value="48"/>
+   <property name="offset_y" type="int" value="-50"/>
+  </properties>
+ </tile>
 </tileset>
