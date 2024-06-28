4a97176612e7a90d1d888e1cbe1b76d0adff7a43
Reid <reidyaro@gmail.com>
2024-06-29 01:00:36 +0200

    Add cave terrain and automap rules

--- data/graphics/tilesets/aemil/cave/Cave.tsx
+++ data/graphics/tilesets/aemil/cave/Cave.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.1" name="Cave" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+<tileset version="1.10" tiledversion="1.10.2" name="Cave" tilewidth="32" tileheight="32" tilecount="256" columns="16">
  <image source="cave.png" width="512" height="512"/>
  <tile id="0">
   <objectgroup draworder="index" id="2">
@@ -783,4 +783,107 @@
    </object>
   </objectgroup>
  </tile>
+ <wangsets>
+  <wangset name="New Set" type="corner" tile="-1">
+   <wangcolor name="Ground 1" color="#ff0000" tile="-1" probability="1"/>
+   <wangcolor name="Ground 2" color="#00ff00" tile="-1" probability="1"/>
+   <wangcolor name="Carpet" color="#0000ff" tile="-1" probability="1"/>
+   <wangcolor name="Water" color="#ff7700" tile="-1" probability="1"/>
+   <wangtile tileid="0" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="1" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="2" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="3" wangid="0,1,0,1,0,0,0,1"/>
+   <wangtile tileid="4" wangid="0,1,0,0,0,1,0,1"/>
+   <wangtile tileid="5" wangid="0,2,0,2,0,0,0,0"/>
+   <wangtile tileid="6" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="7" wangid="0,0,0,0,0,2,0,2"/>
+   <wangtile tileid="8" wangid="0,2,0,2,0,0,0,2"/>
+   <wangtile tileid="9" wangid="0,2,0,0,0,2,0,2"/>
+   <wangtile tileid="11" wangid="0,1,0,2,0,1,0,1"/>
+   <wangtile tileid="12" wangid="0,1,0,2,0,2,0,1"/>
+   <wangtile tileid="13" wangid="0,1,0,1,0,2,0,1"/>
+   <wangtile tileid="14" wangid="0,2,0,2,0,2,0,1"/>
+   <wangtile tileid="15" wangid="0,1,0,2,0,2,0,2"/>
+   <wangtile tileid="16" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="17" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="18" wangid="0,0,0,0,0,0,0,1"/>
+   <wangtile tileid="19" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="20" wangid="0,0,0,0,0,0,0,1"/>
+   <wangtile tileid="21" wangid="0,2,0,0,0,0,0,0"/>
+   <wangtile tileid="22" wangid="0,2,0,0,0,0,0,2"/>
+   <wangtile tileid="23" wangid="0,0,0,0,0,0,0,2"/>
+   <wangtile tileid="24" wangid="0,2,0,0,0,0,0,0"/>
+   <wangtile tileid="25" wangid="0,0,0,0,0,0,0,2"/>
+   <wangtile tileid="27" wangid="0,2,0,2,0,1,0,1"/>
+   <wangtile tileid="28" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="29" wangid="0,1,0,1,0,2,0,2"/>
+   <wangtile tileid="30" wangid="0,2,0,2,0,1,0,2"/>
+   <wangtile tileid="31" wangid="0,2,0,1,0,2,0,2"/>
+   <wangtile tileid="37" wangid="0,0,0,2,0,2,0,2"/>
+   <wangtile tileid="38" wangid="0,0,0,2,0,2,0,0"/>
+   <wangtile tileid="39" wangid="0,2,0,2,0,2,0,0"/>
+   <wangtile tileid="43" wangid="0,2,0,1,0,1,0,1"/>
+   <wangtile tileid="44" wangid="0,2,0,1,0,1,0,2"/>
+   <wangtile tileid="45" wangid="0,1,0,1,0,1,0,2"/>
+   <wangtile tileid="64" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="65" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="66" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="67" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="70" wangid="0,0,0,2,0,0,0,0"/>
+   <wangtile tileid="71" wangid="0,0,0,2,0,2,0,0"/>
+   <wangtile tileid="72" wangid="0,0,0,2,0,2,0,0"/>
+   <wangtile tileid="73" wangid="0,0,0,0,0,2,0,0"/>
+   <wangtile tileid="80" wangid="0,0,0,1,0,1,0,1"/>
+   <wangtile tileid="81" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="82" wangid="0,1,0,1,0,1,0,0"/>
+   <wangtile tileid="83" wangid="0,1,0,4,0,4,0,1"/>
+   <wangtile tileid="86" wangid="0,0,0,2,0,2,0,2"/>
+   <wangtile tileid="87" wangid="0,0,0,2,0,2,0,0"/>
+   <wangtile tileid="88" wangid="0,2,0,2,0,2,0,0"/>
+   <wangtile tileid="89" wangid="0,2,0,4,0,4,0,2"/>
+   <wangtile tileid="96" wangid="0,1,0,4,0,1,0,1"/>
+   <wangtile tileid="97" wangid="0,1,0,4,0,4,0,1"/>
+   <wangtile tileid="98" wangid="0,1,0,1,0,4,0,1"/>
+   <wangtile tileid="99" wangid="0,4,0,1,0,4,0,4"/>
+   <wangtile tileid="100" wangid="0,4,0,4,0,1,0,4"/>
+   <wangtile tileid="102" wangid="0,2,0,4,0,2,0,2"/>
+   <wangtile tileid="103" wangid="0,2,0,4,0,4,0,2"/>
+   <wangtile tileid="104" wangid="0,2,0,2,0,4,0,2"/>
+   <wangtile tileid="105" wangid="0,4,0,2,0,4,0,4"/>
+   <wangtile tileid="106" wangid="0,4,0,4,0,2,0,4"/>
+   <wangtile tileid="112" wangid="0,4,0,4,0,1,0,1"/>
+   <wangtile tileid="113" wangid="0,4,0,4,0,4,0,4"/>
+   <wangtile tileid="114" wangid="0,1,0,1,0,4,0,4"/>
+   <wangtile tileid="115" wangid="0,1,0,4,0,4,0,4"/>
+   <wangtile tileid="116" wangid="0,4,0,4,0,4,0,1"/>
+   <wangtile tileid="118" wangid="0,4,0,4,0,2,0,2"/>
+   <wangtile tileid="120" wangid="0,2,0,2,0,4,0,4"/>
+   <wangtile tileid="121" wangid="0,2,0,4,0,4,0,4"/>
+   <wangtile tileid="122" wangid="0,4,0,4,0,4,0,2"/>
+   <wangtile tileid="128" wangid="0,4,0,1,0,1,0,1"/>
+   <wangtile tileid="129" wangid="0,4,0,1,0,1,0,4"/>
+   <wangtile tileid="130" wangid="0,1,0,1,0,1,0,4"/>
+   <wangtile tileid="134" wangid="0,4,0,2,0,2,0,2"/>
+   <wangtile tileid="135" wangid="0,4,0,2,0,2,0,4"/>
+   <wangtile tileid="136" wangid="0,2,0,2,0,2,0,4"/>
+   <wangtile tileid="198" wangid="0,0,0,3,0,0,0,0"/>
+   <wangtile tileid="199" wangid="0,0,0,3,0,3,0,0"/>
+   <wangtile tileid="200" wangid="0,0,0,0,0,3,0,0"/>
+   <wangtile tileid="201" wangid="0,0,0,3,0,0,0,0"/>
+   <wangtile tileid="202" wangid="0,0,0,3,0,3,0,0"/>
+   <wangtile tileid="203" wangid="0,0,0,0,0,3,0,0"/>
+   <wangtile tileid="214" wangid="0,3,0,3,0,0,0,0"/>
+   <wangtile tileid="215" wangid="0,3,0,3,0,3,0,3"/>
+   <wangtile tileid="216" wangid="0,0,0,0,0,3,0,3"/>
+   <wangtile tileid="217" wangid="0,3,0,3,0,0,0,0"/>
+   <wangtile tileid="218" wangid="0,3,0,3,0,3,0,3"/>
+   <wangtile tileid="219" wangid="0,0,0,0,0,3,0,3"/>
+   <wangtile tileid="230" wangid="0,3,0,0,0,0,0,0"/>
+   <wangtile tileid="231" wangid="0,3,0,0,0,0,0,3"/>
+   <wangtile tileid="232" wangid="0,0,0,0,0,0,0,3"/>
+   <wangtile tileid="233" wangid="0,3,0,0,0,0,0,0"/>
+   <wangtile tileid="234" wangid="0,3,0,0,0,0,0,3"/>
+   <wangtile tileid="235" wangid="0,0,0,0,0,0,0,3"/>
+  </wangset>
+ </wangsets>
 </tileset>
