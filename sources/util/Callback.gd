62b0b3e42b06d09593a08bd7016e243c899fc262
Reid <reidyaro@gmail.com>
2024-07-28 17:21:03 +0200

    Mitigate callback's freed or invalid parameters issues

--- sources/util/Callback.gd
+++ sources/util/Callback.gd
@@ -6,18 +6,25 @@ static func RemoveCallback(objectSignal : Signal, callback : Callable):
 		objectSignal.disconnect(callback)
 
 static func AddCallback(objectSignal : Signal, callback : Callable, args : Array):
-	var callable : Callable = callback.bind(args) if callback == Callback.ShootCallback else callback.bindv(args)
-	objectSignal.connect(callable)
-
-static func ShootCallback(args : Array):
-	if args.size() > 0:
-		var callback : Callable = args.pop_front()
-		if callback:
-			callback.callv(args)
+	var bundledCallback : Callable = callback.bind(args) if callback == Callback.ShootCallback else callback.bindv(args)
+	objectSignal.connect(bundledCallback)
 
 static func PlugCallback(objectSignal : Signal, callback : Callable, args : Array = []):
 	RemoveCallback(objectSignal, callback)
 	AddCallback(objectSignal, callback, args)
+
+static func ShootCallback(args : Array):
+	if args.size() > 0:
+		TriggerCallback(args.pop_front(), args)
+
+static func TriggerCallback(callback : Callable, args : Array = []):
+	if callback and callback is Callable and not callback.is_null() and callback.is_valid():
+		if args.size() == 0 and callback.get_bound_arguments_count() > 0:
+			args = callback.get_bound_arguments()
+		for arg in args:
+			if arg and is_instance_valid(arg) and arg is Object and arg.is_queued_for_deletion():
+				return
+		callback.callv(args)
 
 #
 static func OneShotCallback(objectSignal : Signal, callback : Callable, args : Array = []):
@@ -29,21 +36,19 @@ static func SelfDestructCallback(parent : Node, timer : Timer, callback : Callab
 	if parent:
 		parent.remove_child(timer)
 		timer.queue_free()
-	if not callback.is_null() and callback.is_valid():
-		callback.call()
+	TriggerCallback(callback)
 
 #
 static func SelfDestructTimer(parent : Node, delay : float, callback : Callable = Callable(), timerName = "") -> Timer:
 	if delay <= 0.0:
-		if not callback.is_null() and callback.is_valid():
-			callback.call()
+		TriggerCallback(callback)
 	elif parent:
 		var timer : Timer = null if timerName.is_empty() else parent.get_node_or_null(timerName)
 		if not timer:
 			timer = Timer.new()
 			timer.one_shot = true
 			timer.autostart = true
-			timer.timeout.connect(Callback.SelfDestructCallback.bind(parent, timer, callback))
+			AddCallback(timer.timeout, Callback.SelfDestructCallback, [parent, timer, callback])
 			if not timerName.is_empty():
 				timer.name = timerName
 			parent.add_child(timer)
@@ -52,9 +57,9 @@ static func SelfDestructTimer(parent : Node, delay : float, callback : Callable 
 	return null
 
 #
-static func ResetTimer(timer : Timer, delay : float, callable : Callable):
+static func ResetTimer(timer : Timer, delay : float, callback : Callable):
 	ClearTimer(timer)
-	StartTimer(timer, delay, callable)
+	StartTimer(timer, delay, callback)
 
 static func ClearTimer(timer : Timer):
 	if timer:
@@ -63,15 +68,13 @@ static func ClearTimer(timer : Timer):
 				Callback.RemoveCallback(timer.get(sig["name"]), connection["callable"])
 		timer.stop()
 
-static func StartTimer(timer : Timer, delay : float, callable : Callable):
+static func StartTimer(timer : Timer, delay : float, callback : Callable):
 	if delay == 0.0:
-		callable.call()
+		TriggerCallback(callback)
 	elif timer:
 		timer.start(delay)
 		timer.set_autostart(true)
-		if timer.timeout.is_connected(callable):
-			timer.timeout.disconnect(callable)
-		timer.timeout.connect(callable)
+		PlugCallback(timer.timeout, callback)
 
 static func LoopTimer(timer : Timer, delay : float):
 	Util.Assert(delay > 0, "Delay should never be null, infinite loop can happen on looped timers")
