9ee2f1d5efc3a58be088cc597a44b999cac1ac46
Reid <reidyaro@gmail.com>
2024-08-18 01:51:36 +0200

    Improve sewer's pillars collision shape and re-save the harbour external tileset

--- data/graphics/tilesets/aemil/aurora/artis/sewer/Sewer x1x3.tsx
+++ data/graphics/tilesets/aemil/aurora/artis/sewer/Sewer x1x3.tsx
@@ -3,15 +3,15 @@
  <image source="sewer-x1x3.png" width="512" height="96"/>
  <tile id="6">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="79">
-    <polygon points="0,0 32,0 32,-32 0,-32"/>
+   <object id="1" x="6" y="90">
+    <polygon points="0,0 20,0 20,-20 0,-20"/>
    </object>
   </objectgroup>
  </tile>
  <tile id="8">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="79">
-    <polygon points="0,0 32,0 32,-32 0,-32"/>
+   <object id="2" x="6" y="91">
+    <polygon points="0,0 20,0 20,-20 0,-20"/>
    </object>
   </objectgroup>
  </tile>
