9a03c4214d611d928093e616c7a7e4ba0656713e
Reid <reidyaro@gmail.com>
2024-10-15 02:50:39 +0200

    Use our player nick instead of the node name to check if a player is already connected

--- sources/network/OnlineList.gd
+++ sources/network/OnlineList.gd
@@ -10,7 +10,7 @@ func GetPlayerNames() -> Array[String]:
 		for playerData in Launcher.Network.Server.playerMap.values():
 			var agent : PlayerAgent = WorldAgent.GetAgent(playerData.agentRID)
 			if agent:
-				players.append(agent.get_name())
+				players.append(agent.nick)
 	return players
 
 func UpdateJson():
