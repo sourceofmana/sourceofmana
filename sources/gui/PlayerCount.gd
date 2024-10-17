d5bedc267edbb360ad8b8296b1e9c11475a3babd
NetSysFire <2501566-NetSysFire@users.noreply.gitlab.com>
2024-10-17 16:03:19 +0200

    rename Player Count.gd into PlayerCount.gd, move function to the bottom

---
+++ sources/gui/PlayerCount.gd
@@ -0,0 +1,10 @@
+extends Label
+
+func OnPlayerCountChange():
+	var playercount : int = Launcher.Network.Server.playerMap.values().size()
+	set_text("Current player count: %d" % playercount)
+
+func _ready():
+	set_text("Current player count: 0")
+	Launcher.Network.Server.player_connected.connect(OnPlayerCountChange)
+	Launcher.Network.Server.player_disconnected.connect(OnPlayerCountChange)
