b1e9556101c233e0133cc7fbc20a30f99c9b0b13
Reid <reidyaro@gmail.com>
2025-03-22 00:42:27 +0000

    Fix some wrong and missing collision tiles on drazil and ship maps

--- data/graphics/tilesets/isles/tropical/Ground.tsx
+++ data/graphics/tilesets/isles/tropical/Ground.tsx
@@ -1,11 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="Ground" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+<tileset version="1.10" tiledversion="1.11.2" name="Ground" tilewidth="32" tileheight="32" tilecount="256" columns="16">
  <image source="ground.png" width="512" height="512"/>
- <tile id="152">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="0">
-    <polygon points="0,0 32,0 32,32 0,32"/>
-   </object>
-  </objectgroup>
- </tile>
 </tileset>
