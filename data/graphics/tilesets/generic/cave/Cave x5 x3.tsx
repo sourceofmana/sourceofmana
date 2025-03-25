9480042f1d453778b388afce3d5cfd2dd913808e
Reid <reidyaro@gmail.com>
2025-03-25 18:59:57 +0100

    Change the metal texture on the mine tileset, add a white cave entrace on cave-x5-x3 and do some minor tweaks on the mine entrance

--- data/graphics/tilesets/generic/cave/Cave x5 x3.tsx
+++ data/graphics/tilesets/generic/cave/Cave x5 x3.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.1" name="Cave x5 x3" tilewidth="160" tileheight="96" tilecount="2" columns="2">
- <image source="cave-x5x3.png" width="320" height="96"/>
+<tileset version="1.10" tiledversion="1.10.1" name="Cave x5 x3" tilewidth="160" tileheight="96" tilecount="3" columns="3">
+ <image source="cave-x5x3.png" width="480" height="96"/>
  <tile id="0">
   <objectgroup draworder="index" id="2">
    <object id="3" x="32" y="0">
@@ -18,6 +18,16 @@
    </object>
    <object id="2" x="160" y="32">
     <polygon points="0,0 0,64 -32,64"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <objectgroup draworder="index" id="4">
+   <object id="5" x="32" y="0">
+    <polygon points="0,0 0,64 16,64 16,32 32,0"/>
+   </object>
+   <object id="6" x="144" y="0">
+    <polygon points="16,0 16,64 0,64 0,32 -16,0"/>
    </object>
   </objectgroup>
  </tile>
