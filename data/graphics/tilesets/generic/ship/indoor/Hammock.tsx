981cd8d3935982bfc23bfd72b55b7f4b83c1aca2
Reid <reidyaro@gmail.com>
2025-03-26 03:24:27 +0100

    Add generic bottle and hammock tilesets

---
+++ data/graphics/tilesets/generic/ship/indoor/Hammock.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="Hammock" tilewidth="96" tileheight="64" tilecount="8" columns="4">
+ <image source="hammock.png" width="384" height="128"/>
+</tileset>
