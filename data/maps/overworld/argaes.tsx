00d3dd23a8304fb8d8a2c1bbe59e13c1af942b4b
NetSysFire <59517351+NetSysFire@users.noreply.github.com>
2025-06-04 09:38:45 +0200

    remove the reference tileset

--- data/maps/overworld/argaes.tsx
+++
@@ -1,7 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="argaes" tilewidth="968" tileheight="1032" tilecount="1" columns="0">
- <grid orientation="orthogonal" width="1" height="1"/>
- <tile id="1">
-  <image source="argaes2.png" width="968" height="1032"/>
- </tile>
-</tileset>
