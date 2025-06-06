949d3fc30b67d2719a28caa48b52d97bb4201e1e
Dean Luga <10210776+dluga93@users.noreply.github.com>
2025-06-06 19:45:45 +0200

    #291 Advanced SQL Backups

--- sources/sql/SQLBackups.gd
+++ sources/sql/SQLBackups.gd
@@ -7,49 +7,81 @@ var isRunning : bool					= false
 var stopRequested : bool				= false
 
 #
-func CreateBackup() -> void:
+func CreateDailyBackup() -> String:
 	var date : Dictionary = Time.get_datetime_dict_from_system()
-	var backupFile : String = SQLCommons.GetBackupPath() + "%d-%02d-%02d_%02d-%02d-%02d" % [date.year, date.month, date.day, date.hour, date.minute, date.second] + Path.DBExt
+	var frequencyDir : String = SQLCommons.BackupFrequency.keys()[SQLCommons.BackupFrequency.DAILY]
+	var backupFile : String = SQLCommons.GetBackupPath() + "%s/%d-%02d-%02d_%02d-%02d-%02d" % [frequencyDir, date.year, date.month, date.day, date.hour, date.minute, date.second] + Path.DBExt
 	if Launcher.SQL.db.backup_to(backupFile):
 		Util.PrintInfo("SQL", "Backup created: " + backupFile)
+		return backupFile
 	else:
 		Util.PrintLog("SQL", "Backup failed: " + backupFile)
+		return ""
+
+func CopyBackup(backupFilePath : String, backupFrequency : SQLCommons.BackupFrequency) -> String:
+	var dir : DirAccess = DirAccess.open(SQLCommons.GetBackupPath())
+
+	var frequencyDir : String = SQLCommons.BackupFrequency.keys()[backupFrequency]
+	var newFile : String = SQLCommons.GetBackupPath() + "%s/%s" % [frequencyDir, backupFilePath.get_file()]
+	var errorCode : Error = dir.copy(backupFilePath, newFile)
+
+	if (errorCode == Error.OK):
+		Util.PrintInfo("SQL", "Backup created: " + newFile)
+		return newFile
+	else:
+		Util.PrintLog("SQL", "Backup failed for file %s with code %d" % [newFile, errorCode])
+		return ""
 
 func PruneBackups() -> void:
-	var dir : DirAccess = DirAccess.open(SQLCommons.GetBackupPath())
-	if not dir:
-		return
-	
-	var dirFiles : PackedStringArray = dir.get_files()
-	var backupFiles : Array[String] = []
-	for file in dirFiles:
-		if file.get_extension() == "db":
-			backupFiles.append(file)
+	for backupFrequency in SQLCommons.BackupFrequency.values():
+		var backupFrequencyDir = SQLCommons.BackupFrequency.keys()[backupFrequency]
+		var dir : DirAccess = DirAccess.open(SQLCommons.GetBackupPath() + "/" + backupFrequencyDir)
+		if not dir:
+			return
+		
+		var dirFiles : PackedStringArray = dir.get_files()
+		var backupFiles : Array[String] = []
+		for file in dirFiles:
+			if file.get_extension() == "db":
+				backupFiles.append(file)
 
-		backupFiles.sort() # Oldest backups first
-		while backupFiles.size() > SQLCommons.BackupLimit:
-			var prunedFile : String = backupFiles.pop_front()
-			var err : Error = dir.remove(prunedFile)
-			if err == OK:
-				Util.PrintInfo("SQL", "Backup removed: " + prunedFile)
-			else:
-				Util.PrintLog("SQL", "Backup removal failed: %s [%d]" % [prunedFile, err])
+			backupFiles.sort() # Oldest backups first
+			while backupFiles.size() > SQLCommons.BackupLimits[backupFrequency]:
+				var prunedFile : String = backupFiles.pop_front()
+				var err : Error = dir.remove(prunedFile)
+				if err == OK:
+					Util.PrintInfo("SQL", "Backup removed: " + prunedFile)
+				else:
+					Util.PrintLog("SQL", "Backup removal failed: %s [%d]" % [prunedFile, err])
 
 #
 func Run():
 	Thread.set_thread_safety_checks_enabled(false)
 
-	var lastBackupTimestamp : int = SQLCommons.Timestamp()
+	var lastDailyBackupTimestamp : int = SQLCommons.Timestamp()
+	var lastWeeklyBackupTimestamp : int = SQLCommons.Timestamp()
+	var lastMonthlyBackupTimestamp : int = SQLCommons.Timestamp()
 	var lastPlayerUpdateTimestamp : int = SQLCommons.Timestamp()
 	var lastStopCheckTimestamp : int = SQLCommons.Timestamp()
 
 	while isRunning:
 		var timestamp : int = SQLCommons.Timestamp()
 
-		if timestamp - lastBackupTimestamp >= SQLCommons.BackupIntervalSec:
-			CreateBackup()
+		if timestamp - lastDailyBackupTimestamp >= SQLCommons.DailyBackupIntervalSec:
+			var backupFilePath: String = CreateDailyBackup()
+			lastDailyBackupTimestamp = timestamp
+
+			if timestamp - lastWeeklyBackupTimestamp >= SQLCommons.WeeklyBackupIntervalSec \
+					and !backupFilePath.is_empty():
+				backupFilePath = CopyBackup(backupFilePath, SQLCommons.BackupFrequency.WEEKLY)
+				lastWeeklyBackupTimestamp = timestamp
+			
+			if timestamp - lastMonthlyBackupTimestamp >= SQLCommons.MonthlyBackupIntervalSec \
+					and !backupFilePath.is_empty():
+				CopyBackup(backupFilePath, SQLCommons.BackupFrequency.MONTHLY)
+				lastMonthlyBackupTimestamp = timestamp
+
 			PruneBackups()
-			lastBackupTimestamp = timestamp
 
 		if timestamp - lastPlayerUpdateTimestamp >= SQLCommons.BackupPlayersSec:
 			if Launcher.World:
