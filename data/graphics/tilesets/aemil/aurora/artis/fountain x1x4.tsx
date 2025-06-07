773f794c8f6fd2569886aed014204b92ad2a5b9a
NetSysFire <59517351+NetSysFire@users.noreply.github.com>
2025-06-07 12:45:50 +0200

    reduce chimney particle speed, adjust fountain effect, give particle nodes names (#311)
    * give particle effects proper node names, slow down the chimney smoke, rename typoed 'foutain' tileset, adjust fountain particles
    
    * Increase the pre-process of the chimney smoke to match the extended lifetime
    
    ---------
    
    Co-authored-by: Reid <reidyaro@gmail.com>

--- data/graphics/tilesets/aemil/aurora/artis/fountain x1x4.tsx
+++ data/graphics/tilesets/aemil/aurora/artis/fountain x1x4.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.1" name="Foutain x1x4" tilewidth="32" tileheight="128" tilecount="11" columns="11">
+<tileset version="1.10" tiledversion="1.10.1" name="Fountain x1x4" tilewidth="32" tileheight="128" tilecount="11" columns="11">
  <image source="fountain-x1x4.png" width="352" height="128"/>
  <tile id="5">
   <properties>
