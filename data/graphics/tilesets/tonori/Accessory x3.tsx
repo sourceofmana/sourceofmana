38cb598565becc4ac9992dedd6209894d37bffea
Reid <reidyaro@gmail.com>
2025-10-20 01:45:03 +0100

    Add foliage wind effect for accessory x3 and palm tree tilesets

--- data/graphics/tilesets/tonori/Accessory x3.tsx
+++ data/graphics/tilesets/tonori/Accessory x3.tsx
@@ -1,4 +1,14 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.0" name="Accessory x3" tilewidth="32" tileheight="96" tilecount="16" columns="16">
  <image source="accessory-x1x3.png" width="512" height="96"/>
+ <tile id="1">
+  <properties>
+   <property name="Material" value="tonori/accessory-x3"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="Material" value="tonori/accessory-x3"/>
+  </properties>
+ </tile>
 </tileset>
--- data/graphics/tilesets/tonori/Palm Tree x5.tsx
+++ data/graphics/tilesets/tonori/Palm Tree x5.tsx
@@ -1,4 +1,49 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.0" name="Palm Tree x5" tilewidth="32" tileheight="160" tilecount="16" columns="16">
  <image source="palm-tree-x5.png" width="512" height="160"/>
+ <tile id="0">
+  <properties>
+   <property name="Material" value="tonori/palm-tree-x5"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="Material" value="tonori/palm-tree-x5"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="Material" value="tonori/palm-tree-x5"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="Material" value="tonori/palm-tree-x5"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="Material" value="tonori/palm-tree-x5"/>
+  </properties>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="Material" value="tonori/palm-tree-x5"/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="Material" value="tonori/palm-tree-x5"/>
+  </properties>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="Material" value="tonori/palm-tree-x5"/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="Material" value="tonori/palm-tree-x5"/>
+  </properties>
+ </tile>
 </tileset>
