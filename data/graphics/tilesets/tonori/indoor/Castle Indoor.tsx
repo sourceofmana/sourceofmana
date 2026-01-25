d878e4b631d8f97db0bba12ed2f180c8f8cfe4d3
Reid <reidyaro@gmail.com>
2026-01-25 02:53:21 +0000

    Add new castle corridors maps, integrate lighting system and new decoration tiles

--- data/graphics/tilesets/tonori/indoor/Castle Indoor.tsx
+++ data/graphics/tilesets/tonori/indoor/Castle Indoor.tsx
@@ -1,4 +1,17 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.0" name="Castle Indoor" tilewidth="32" tileheight="32" tilecount="256" columns="16">
  <image source="castle-indoor.png" width="512" height="512"/>
+ <tile id="224">
+  <properties>
+   <property name="custom" value="LightSource"/>
+   <property name="light_color" value="FFD28DFF"/>
+   <property name="light_offset" type="int" value="24"/>
+   <property name="light_radius" type="int" value="128"/>
+  </properties>
+  <animation>
+   <frame tileid="224" duration="100"/>
+   <frame tileid="225" duration="100"/>
+   <frame tileid="226" duration="100"/>
+  </animation>
+ </tile>
 </tileset>
---
+++ data/graphics/tilesets/tonori/indoor/Sunlight x4 x4.tsx
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Sunlight x4 x4" tilewidth="128" tileheight="128" tilecount="2" columns="2">
+ <image source="sunlight-x4-x4.png" width="256" height="128"/>
+ <tile id="0">
+  <properties>
+   <property name="custom" value="LightSource"/>
+   <property name="light_color" value="FFFFFFAA"/>
+   <property name="light_radius" type="int" value="256"/>
+   <property name="light_speed" type="float" value="0"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="custom" value="LightSource"/>
+   <property name="light_color" value="FFFFFFAA"/>
+   <property name="light_radius" type="int" value="256"/>
+   <property name="light_speed" type="float" value="0"/>
+  </properties>
+ </tile>
+</tileset>
