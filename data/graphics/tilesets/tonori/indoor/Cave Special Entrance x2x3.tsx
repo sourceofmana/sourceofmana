669c82d1ecaf41532bedfa1000a55570fee37705
Reid <reidyaro@gmail.com>
2026-02-16 22:49:54 +0000

    Add a specific cave entrance/exit for the deepest mine level and rework some light positions

---
+++ data/graphics/tilesets/tonori/indoor/Cave Special Entrance x2x3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Cave Special Entrance x2x3" tilewidth="64" tileheight="96" tilecount="2" columns="2">
+ <image source="../cave/cave-special-entrance-x2x3.png" width="128" height="96"/>
+</tileset>
