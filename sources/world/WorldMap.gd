aebe5b485417aa273fd1f84886701576feac9a03
Reid <reidyaro@gmail.com>
2026-04-05 14:40:59 +0100

    Refactor our spawn object to be serialized, unforce the need to write count=1, it will be the default value for spawn objects and remove the has_trigger field as we just check for the trigger_radius instead

--- sources/world/WorldMap.gd
+++ sources/world/WorldMap.gd
@@ -57,28 +57,6 @@ func LoadMapData():
 		for spawn in resource.spawns:
 			assert(spawn != null, "Spawn format is not supported")
 			if spawn:
-				var spawnObject = SpawnObject.new()
-				spawnObject.count = spawn[0]
-				spawnObject.id = spawn[1]
-				spawnObject.type = spawn[2]
-				spawnObject.spawn_position = spawn[3]
-				spawnObject.spawn_offset = spawn[4]
-				spawnObject.respawn_delay = spawn[5]
-				spawnObject.player_script = spawn[6]
-				spawnObject.own_script = spawn[7]
-				spawnObject.nick = spawn[8]
-				spawnObject.is_always_visible = spawn[9] if spawn.size() > 9 else false
-				spawnObject.direction = spawn[10] if spawn.size() > 10 else ActorCommons.Direction.UNKNOWN
-				spawnObject.state = spawn[11] if spawn.size() > 11 else ActorCommons.State.UNKNOWN
-				spawnObject.has_trigger = spawn[12] if spawn.size() > 12 else false
-				spawnObject.trigger_radius = spawn[13] if spawn.size() > 13 else 0.0
-				spawnObject.trigger_polygon = spawn[14] if spawn.size() > 14 else PackedVector2Array()
-				spawnObject.destination_map = spawn[15] if spawn.size() > 15 else DB.UnknownHash
-				spawnObject.destination_pos = spawn[16] if spawn.size() > 16 else Vector2.ZERO
-				spawnObject.auto_warp = spawn[17] if spawn.size() > 17 else true
-				spawnObject.sailing_pos = spawn[18] if spawn.size() > 18 else Vector2.ZERO
-				spawnObject.is_global = spawnObject.spawn_position < Vector2i.LEFT
-				spawnObject.is_persistant = true
-				spawnObject.map = self
-				spawns.append(spawnObject)
+				spawn.map = self
+				spawns.append(spawn)
 func HasFlags(checkedFlags : Flags) -> bool: return !!(flags & checkedFlags)
