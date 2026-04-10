c988e7553e08706b46d5a5523f50b4dbe2b1cd6a
Reid <reidyaro@gmail.com>
2026-04-10 01:32:44 +0100

    Deprecate the physics update on Projectile to use a tween instead

--- sources/effects/Projectile.gd
+++ sources/effects/Projectile.gd
@@ -2,31 +2,36 @@ extends Node2D
 class_name Projectile
 
 #
-@export var canRotate : bool				= false
+@export var canRotate : bool			= false
 
 var origin : Vector2					= Vector2.ZERO
 var destination : Vector2				= Vector2.ZERO
 var delay : float						= 0.0
-var elapsed : float						= 0.0
 var fade : float						= 0.15
 var light : LightSource					= null
 
 #
-func _physics_process(delta):
-	elapsed = min(delay, elapsed + delta)
-
-	if elapsed == delay:
-		Util.RemoveNode(self, get_parent())
-	else:
-		var scaleRatio : float = Util.FadeInOutRatio(elapsed, delay, fade, fade)
-		if light:
-			light.rescale = scaleRatio
-		scale = lerp(Vector2.ZERO, Vector2.ONE, scaleRatio)
-		global_position = lerp(origin, destination, elapsed / delay)
-
 func _ready():
 	if has_node("LightSource"):
 		light = get_node("LightSource")
 	if canRotate:
 		rotation = origin.angle_to_point(destination)
-	set("set_speed_scale", delay)
+	if delay <= 0.0:
+		return
+
+	scale = Vector2.ZERO
+	global_position = origin
+
+	var tween : Tween = create_tween()
+	tween.set_parallel(true)
+	tween.tween_property(self, "global_position", destination, delay).from(origin)
+	tween.tween_method(UpdateScale, 0.0, delay, delay)
+	tween.set_parallel(false)
+	tween.tween_callback(Util.RemoveNode.bind(self, get_parent()))
+
+#
+func UpdateScale(elapsed : float) -> void:
+	var scaleRatio : float = Util.FadeInOutRatio(elapsed, delay, fade, fade)
+	if light:
+		light.rescale = scaleRatio
+	scale = lerp(Vector2.ZERO, Vector2.ONE, scaleRatio)
