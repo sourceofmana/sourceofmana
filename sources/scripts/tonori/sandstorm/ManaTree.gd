c7ed8d79a08b54a539f7de4c36e0b0209818bf84
WildX <wildx@themanaworld.org>
2026-07-08 10:32:55 +0100

    Fix dialogue formatting in ManaTree.gd

--- sources/scripts/tonori/sandstorm/ManaTree.gd
+++ sources/scripts/tonori/sandstorm/ManaTree.gd
@@ -16,7 +16,7 @@ func OnPreFightIntro():
 	Narrate("You notice a crack in the cave wall. A faint blue light seems to come from the other side.")
 	Narrate("It looks like you can squeeze through.")
 	Narrate("A calm voice fills your head.")
-	Mes("I don't feel the same dark presence that I felt with the first one who came through here. Please come forward. 
+	Mes("I don't feel the same dark presence that I felt with the first one who came through here. Please come forward.")
 	Mes("For the first time in thousands of years, I will be needing someone's help.")
 
 func OnPostFight():
