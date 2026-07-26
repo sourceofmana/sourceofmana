d10695075310344f3b4680433c31aa8563c1e954
Reid <reidyaro@gmail.com>
2026-07-26 21:46:13 +0100

    Remove the overworld warp from Nard's script and use placeholder lines until we integrate its own quest line

--- sources/scripts/ship/Nard.gd
+++ sources/scripts/ship/Nard.gd
@@ -2,15 +2,5 @@ extends NpcScript
 
 #
 func OnStart():
-	Mes("Ready to head back out on the water?")
-	Choice("Yes, let's sail!", OnSail)
-	Choice("Not yet.", Dismiss)
-
-func OnSail():
-	var player : PlayerAgent = own as PlayerAgent
-	if player and player.exploreOrigin:
-		player.Morph(false, DB.ShipHash)
-		Warp("Overworld".hash(), player.exploreOrigin.pos)
-
-func Dismiss():
-	Chat("Take your time.")
+	Mes("You want to keep sailing?")
+	Mes("Take the helm on the upper deck. Me and the crew will handle the sails.")
