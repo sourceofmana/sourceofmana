530001249c1eb171d6c24fa76750228d425911c2
Reid <reidyaro@gmail.com>
2026-08-10 01:56:42 +0100

    Prevent to return to the deep level desert map if the Mine Exploration quest is completed

--- sources/scripts/tonori/sandstorm/DeepLevelEntrance.gd
+++ sources/scripts/tonori/sandstorm/DeepLevelEntrance.gd
@@ -1,8 +1,17 @@
 extends NpcScript
 
+#
 const QUEST_ID : int = ProgressCommons.Quest.MINE_EXPLORATION
 
+#
 func OnStart():
+	if not own or not own.progress:
+		return
+
+	var questState : int = own.progress.GetQuest(ProgressCommons.Quest.MINE_EXPLORATION)
+	if questState < ProgressCommons.MINE_EXPLORATION.STRANGER_SPOTTED or questState == ProgressCommons.CompletedProgress:
+		return
+
 	Narrate("You notice a crack in the cave wall. A faint blue light seems to come from the other side.")
 	Narrate("It looks like you can squeeze through.")
 	Narrate("A calm voice fills your head.")
