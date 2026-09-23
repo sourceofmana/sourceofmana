029576104af658b76c6a5a72a070bde5e1c4ef44
Reid <reidyaro@gmail.com>
2026-09-23 02:38:44 +0100

    Autofit the notification label depending of the notification text

--- sources/gui/Notification.gd
+++ sources/gui/Notification.gd
@@ -2,19 +2,32 @@ extends Control
 
 @export var openSpeed : float	= 4.0
 @export var closeSpeed : float	= 1.5
+@export var maxWidth : float	= 500.0
 
 @onready var clip : Control			= $ClipContainer
 @onready var label : RichTextLabel	= $ClipContainer/Label
 @onready var timer : Timer			= $Timer
 
-const HALF_WIDTH : float = 250.0
+var halfWidth : float = maxWidth * 0.5
 var tween : Tween = null
 
 #
 func _get_minimum_size() -> Vector2:
 	if label == null:
 		return Vector2.ZERO
-	return Vector2(HALF_WIDTH * 2.0, label.get_minimum_size().y)
+	return Vector2(halfWidth * 2.0, label.get_minimum_size().y)
+
+#
+func FitLabel(notif : String) -> float:
+	var wrapMode : TextServer.AutowrapMode = label.autowrap_mode
+	label.autowrap_mode = TextServer.AUTOWRAP_OFF
+	label.text = "[center]%s[/center]" % notif
+
+	var half : float = minf(label.get_minimum_size().x, maxWidth) * 0.5
+	label.autowrap_mode = wrapMode
+	label.offset_left = -half
+	label.offset_right = half
+	return half
 
 #
 func AddNotification(notif : String, delay : float = 5.0):
@@ -22,7 +35,7 @@ func AddNotification(notif : String, delay : float = 5.0):
 		if not timer.is_stopped():
 			timer.stop()
 		show()
-		label.text = "[center]%s[/center]" % notif
+		halfWidth = FitLabel(notif)
 		update_minimum_size()
 		timer.start(delay)
 		Animate(1.0, openSpeed)
@@ -37,8 +50,8 @@ func Animate(target : float, speed : float) -> void:
 	if tween:
 		tween.kill()
 	var current_half : float = clip.offset_right
-	var target_half : float = HALF_WIDTH * target
-	var duration : float = absf(target_half - current_half) / (HALF_WIDTH * speed)
+	var target_half : float = halfWidth * target
+	var duration : float = absf(target_half - current_half) / (halfWidth * speed)
 	tween = create_tween()
 	tween.tween_method(
 		func(v : float):
